fca1ec039440554da33b51b6acc28bb4367122c9
Dmitry Indenbom <dmitry.indenbom@yandex.ru>
2019-09-29 22:07:04 +0300

    Поправил, то что просили, и всё прокомментировал.

--- Program B1/Program B1/Main.cpp
+++ Program B1/Program B1/Main.cpp
@@ -1,37 +1,52 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <bitset>
 
+constexpr size_t alphabet_size = 26;
 
 
-void reverse_pre_function(std::vector<int>& prefix_value, std::string& answer) {
-	int last_success = -1; //Номер последнего символа последнего ненулевого префикса
-	char base_char; //Символ, который будет вводится при нулевом суффиксе
-	std::bitset<30> used(1); //Множество запрещённых символов
-	answer = 'a';
+
+std::string reverse_pre_function(std::vector<int>& prefix_value) {
+	//Номер последнего символа последнего ненулевого префикса, совпадающего с суффиксом
+	int last_success = -1;
+	std::bitset<alphabet_size> used(1); //Множество запрещённых символов
+	std::string answer = "a"; //Так как первое значение префикс функции всегда -1
 	for (int i = 1; i < prefix_value.size(); i++) {
 		if (prefix_value[i] == 0) {
 			while (last_success > -1) {
+				/*
+				Символ нельзя использовать, иначе будет ненулевой суффикс, совпадающий с префиксом.
+				За хранение его "номера - 1" и отвечает last_success
+				*/
 				used[answer[last_success + 1] - 'a'] = 1;
+				//Ищем такие символы в подсуффиксах
 				last_success = prefix_value[last_success] - 1;
 			}
+			//Ищем минимальный незапрещённый символ
 			int j = 0;
-			while (j < used.size()) {
+			for (; j < used.size(); j++) {
 				if (used[j] == 0)
 					break;
-				j++;
 			}
-			base_char = 'a' + j;
-			answer += base_char;
+			//Добавляем символ в строку
+			answer += ('a' + j);
+			//Префикса нет, поэтому last_success сбрасывается
 			last_success = -1;
+			//Так как 'a' всегда первый символ, он всегда запрещён
 			used.reset();
 			used.set(0);
 		}
 		else {
+			/*
+			Чтобы правильно определить следующую букву,
+			нужно запомнить положение предыдущей в префиксе
+			*/
 			last_success = prefix_value[i] - 1;
 			answer += answer[last_success];
 		}
 	}
+
+	return answer;
 }
 
 
@@ -42,8 +57,7 @@ int main() {
 	while (std::cin >> a)
 		prefix_value.push_back(a);
 	if (prefix_value.size() != 0) {
-		std::string answer;
-		reverse_pre_function(prefix_value, answer);
+		std::string answer = reverse_pre_function(prefix_value);
 		std::cout << answer;
 	}
 
