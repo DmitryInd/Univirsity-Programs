aa5ffe5ab049e2e673769b778bec5725adb365ce
Dmitry Indenbom <dmitry.indenbom@yandex.ru>
2019-12-18 22:14:32 +0300

    Внесены правки, связанные с точками и алгоритмом Чана (исправления взяты из моего решения задачи B, так как она является подзадачей текущей).

--- Contest 28_10_19/Program D/Program D/Main.cpp
+++ Contest 28_10_19/Program D/Program D/Main.cpp
@@ -1,53 +1,46 @@
-п»ї#include<iostream>
+#include<iostream>
 #include<vector>
 #include<array>
 #include<cfloat>
 #include<math.h>
 #include<algorithm>
 
-constexpr double base_angle = 0.000001;
+constexpr double base_angle = 0.00000001;
 
 
-
-//РўРѕС‡РєР°(РІРµРєС‚РѕСЂ) РІ РїСЂРѕСЃС‚СЂР°РЅСЃС‚РІРµ
+//Точка(вектор) в пространстве
 struct point {
 	double x = DBL_MAX;
 	double y = DBL_MAX;
 	double z = DBL_MAX;
-	point* prev = nullptr;
-	point* next = nullptr;
 
-	int index = -1;
-	int degree = 0;
-	
-	point(double a, double b, double c) { x = a; y = b; z = c; }
-	point(double a, double b) { x = a; y = b; z = pow(a, 2) + pow(b, 2); }
-	point() = default;
-	void step();
 	void rotate(double angle);
 
-	bool operator<(const point& other) const { return x < other.x; };
 	point& operator=(const point& other) = default;
 	point operator-(const point& other) const
 	{
-		return point(x - other.x, y - other.y, z - other.z);
+		return point{ x - other.x, y - other.y, z - other.z };
 	}
 	point operator+(const point& other) const
 	{
-		return point(x + other.x, y + other.y, z + other.z);
-	}
-	point operator*(double k) const
-	{
-		return point(x * k, y * k, z * k);
-	}
-	point operator/(double k) const
-	{
-		return point(x / k, y / k, z / k);
+		return point{ x + other.x, y + other.y, z + other.z };
 	}
 };
 
-//РџРѕРїР°РґР°РЅРёРµ РІ РѕР±РѕР»РѕС‡РєСѓ Рё РІС‹С…РѕРґ РёР· РЅРµС‘
-void point::step() {
+struct node_hull {
+	point coord;
+	node_hull* prev = nullptr;
+	node_hull* next = nullptr;
+
+	node_hull& operator=(const node_hull& other) = default;
+	int degree = 0;
+	int index = -1;
+
+	void step();
+};
+
+//Попадание в оболочку и выход из неё
+void node_hull::step() {
 	if (prev->next == this) {
 		prev->next = next;
 		next->prev = prev;
@@ -58,79 +51,89 @@ void point::step() {
 	}
 }
 
-//РџРѕРІРѕСЂРѕС‚ СЂР°РґРёСѓСЃ РІРµРєС‚РѕСЂР° С‚РѕС‡РєРё РЅР° Р·Р°РґР°РЅРЅС‹Р№ СѓРіР»
+/*Поворот радиус вектора точки на заданный угл вокруг всех осей для
+выполнения условий корректной работы алгоритма Чана.
+(Никакие три точки не должны лежать в одной вертикальной плоскости)*/
 void point::rotate(double angle) {
 	double x_next, y_next, z_next;
-	//РџРѕРІРѕСЂРѕС‚ РІРѕРєСЂСѓРі РѕСЃРё x
+	//Поворот вокруг оси x
 	z_next = z * cos(angle) + y * sin(angle);
 	y_next = -z * sin(angle) + y * cos(angle);
 	z = z_next;
 	y = y_next;
-	//РџРѕРІРѕСЂРѕС‚ РІРѕРєСЂСѓРі РѕСЃРё y
+	//Поворот вокруг оси y
 	x_next = x * cos(angle) + z * sin(angle);
 	z_next = -x * sin(angle) + z * cos(angle);
 	x = x_next;
 	z = z_next;
 
-	//РџРѕРІРѕСЂРѕС‚ РІРѕРєСЂСѓРі РѕСЃРё z
+	//Поворот вокруг оси z
 	x_next = x * cos(angle) + y * sin(angle);
 	y_next = -x * sin(angle) + y * cos(angle);
 	x = x_next;
 	y = y_next;
 }
 
-/*Р’РµРєС‚РѕСЂРЅРѕРµ РїСЂРѕРёР·РІРµРґРµРЅРёРµ РґРІСѓС… РІРµРєС‚РѕСЂРѕРІ, РѕРїСЂРµРґРµР»С‘РЅРЅС‹С… С‚СЂРµРјСЏ С‚РѕС‡РєР°РјРё, РІ РїСЂРѕРµРєС†РёРё РЅР° РїР»РѕСЃРєРѕСЃС‚СЊ xy
-РёР»Рё Р·РЅР°Рє РІРµРєС‚РѕСЂРЅРѕРіРѕ РїСЂРѕРёР·РІРµРґРµРЅРёСЏ РІ РїСЂРѕРµРєС†РёРё Р§Р°РЅР° РїСЂРё t = -INF*/
-double initial_vector_product(const point* first_p, const point* second_p, const point* third_p) {
-	if (first_p == nullptr || second_p == nullptr || third_p == nullptr ||
-		first_p->index == -1 || second_p->index == -1 || third_p->index == -1) {
+//Компоратор для сортировки по оси x
+struct compare_by_x {
+	bool operator()(const node_hull& first, const node_hull& second) {
+		return first.coord.x < second.coord.x;
+	}
+};
+
+//Проверка на существование/фиктивность точки
+bool is_point_exist(const node_hull* point_ptr) {
+	return (point_ptr != nullptr && point_ptr->index != -1);
+}
+
+/*Векторное произведение двух векторов, определённых тремя точками, в проекции на плоскость xy
+или знак векторного произведения в проекции Чана при t = -INF*/
+double initial_vector_product(const node_hull* first_p, const node_hull* second_p, const node_hull* third_p) {
+	if (!(is_point_exist(first_p) && is_point_exist(second_p) && is_point_exist(third_p))) {
 		return 1;
 	}
-	point first_vector = *second_p - *first_p;
-	point second_vector = *third_p - *first_p;
+	point first_vector = second_p->coord - first_p->coord;
+	point second_vector = third_p->coord - first_p->coord;
 	return first_vector.x * second_vector.y - second_vector.x * first_vector.y;
 }
 
-/*Р’РµРєС‚РѕСЂРЅРѕРµ РїСЂРѕРёР·РІРµРґРµРЅРёРµ РґРІСѓС… РІРµРєС‚РѕСЂРѕРІ РІ РїСЂРѕРµРєС†РёРё РЅР° РїР»РѕСЃРєРѕСЃС‚СЊ xy
-РёР»Рё Р·РЅР°Рє РІРµРєС‚РѕСЂРЅРѕРіРѕ РїСЂРѕРёР·РІРµРґРµРЅРёСЏ РІ РїСЂРѕРµРєС†РёРё Р§Р°РЅР° РїСЂРё t = -INF*/
+/*Векторное произведение двух векторов в проекции на плоскость xy
+или знак векторного произведения в проекции Чана при t = -INF*/
 double initial_vector_product(const point* first_vector, const point* second_vector) {
 	return first_vector->x * second_vector->y - second_vector->x * first_vector->y;
 }
 
-//РџРѕРёСЃРє РјРѕРјРµРЅС‚Р°, РІ РєРѕС‚РѕСЂС‹Р№ РІС‚РѕСЂР°СЏ С‚РѕС‡РєР° РїРѕРїР°РґС‘С‚ РІ РѕР±РѕР»РѕС‡РєСѓ
-double search_intersection_time(const point* first_p, const point* second_p, const point* third_p) {
-	if (first_p == nullptr || second_p == nullptr || third_p == nullptr ||
-		first_p->index == -1 || second_p->index == -1 || third_p->index == -1) {
+//Поиск момента, в который вторая точка попадёт в оболочку
+double search_intersection_time(const node_hull* first_p, const node_hull* second_p, const node_hull* third_p) {
+	if (!(is_point_exist(first_p) && is_point_exist(second_p) && is_point_exist(third_p))) {
 		return DBL_MAX;
 	}
-	point first_vector = *second_p - *first_p;
-	point second_vector = *third_p - *first_p;
+	point first_vector = second_p->coord - first_p->coord;
+	point second_vector = third_p->coord - first_p->coord;
 	return (first_vector.x * second_vector.z - second_vector.x * first_vector.z)
 		/ initial_vector_product(&first_vector, &second_vector);
 }
 
-//РџРѕРёСЃРє РЅРёР¶РЅРµР№ С‡Р°СЃС‚Рё РѕР±РѕР»РѕС‡РєРё
-std::vector<point*> search_down_hull(std::vector<point>& vertices, int size, point* zero, int begin = 0) {
+//Поиск нижней части оболочки
+std::vector<node_hull*> find_down_hull(std::vector<node_hull>& vertices, int size, node_hull* zero, int begin = 0) {
 	if (size <= 1) {
-		std::vector<point*> entire_hull;
+		std::vector<node_hull*> entire_hull;
 		entire_hull.push_back(zero);
 		vertices[begin].next = zero;
 		vertices[begin].prev = zero;
 		return entire_hull;
 	}
 
-	//РўРѕС‡РєРё, СЃРѕСЃС‚РѕРІР»СЏСЋС‰РёРµ РјРѕСЃС‚
-	point* u;
-	point* v;
-	u = &vertices[begin + size / 2 - 1];
-	v = &vertices[begin + size / 2];
-	point* midle = v;
-	//Р Р°Р·Р±РёРµРЅРёРµ РѕР±РѕР»РѕС‡РєРё РЅР° РґРІРµ С‡Р°СЃС‚Рё
-	std::vector<point*> first_half_hull = search_down_hull(vertices, size / 2, zero, begin);
-	std::vector<point*> second_half_hull = search_down_hull(vertices, size - size / 2, zero, begin + size / 2);
-	std::vector<point*> entire_hull;
+	//Точки, состовляющие мост
+	node_hull* u = &vertices[begin + size / 2 - 1];
+	node_hull* v = &vertices[begin + size / 2];
+	node_hull* midle = v;
+	//Разбиение оболочки на две части
+	std::vector<node_hull*> first_half_hull = find_down_hull(vertices, size / 2, zero, begin);
+	std::vector<node_hull*> second_half_hull = find_down_hull(vertices, size - size / 2, zero, begin + size / 2);
+	std::vector<node_hull*> entire_hull;
 
-	//РџРѕРёСЃРє РјРѕСЃС‚Р° РїСЂРё t = -INF
+	//Поиск моста при t = -INF
 	while (true) {
 		if (initial_vector_product(u, v, v->next) < 0) {
 			v = v->next;
@@ -146,7 +149,7 @@ std::vector<point*> search_down_hull(std::vector<point>& vertices, int size, poi
 	double first_time = -DBL_MAX;
 	double second_time;
 	std::array<double, 6> certain_time;
-	//РЎР»РёСЏРЅРёРµ
+	//Слияние
 	for (int i = 0, j = 0; ; first_time = second_time) {
 		certain_time[0] = search_intersection_time(first_half_hull[i]->prev, first_half_hull[i], first_half_hull[i]->next);
 		certain_time[1] = search_intersection_time(second_half_hull[j]->prev, second_half_hull[j], second_half_hull[j]->next);
@@ -154,7 +157,7 @@ std::vector<point*> search_down_hull(std::vector<point>& vertices, int size, poi
 		certain_time[3] = search_intersection_time(u->prev, u, v);
 		certain_time[4] = search_intersection_time(u, v, v->next);
 		certain_time[5] = search_intersection_time(u, v->prev, v);
-		//РџРѕРёСЃРє Р±Р»РёР¶Р°Р№С€РµРіРѕ СЃРѕР±С‹С‚РёСЏ
+		//Поиск ближайшего события
 		second_time = DBL_MAX;
 		int event_number = -1;
 		for (int h = 0; h < 6; h++) {
@@ -167,13 +170,13 @@ std::vector<point*> search_down_hull(std::vector<point>& vertices, int size, poi
 			break;
 		}
 		else if (event_number == 0) {
-			if (first_half_hull[i]->x < u->x) {
+			if (first_half_hull[i]->coord.x < u->coord.x) {
 				entire_hull.push_back(first_half_hull[i]);
 			}
 			first_half_hull[i++]->step();
 		}
 		else if (event_number == 1) {
-			if (second_half_hull[j]->x > v->x) {
+			if (second_half_hull[j]->coord.x > v->coord.x) {
 				entire_hull.push_back(second_half_hull[j]);
 			}
 			second_half_hull[j++]->step();
@@ -197,12 +200,11 @@ std::vector<point*> search_down_hull(std::vector<point>& vertices, int size, poi
 	}
 	entire_hull.push_back(zero);
 
-	//РЎРѕР·РґР°РЅРёРµ РјРѕСЃС‚Р° Рё РІРѕР·РІСЂР°С‚ Рє t = -INF СЃ РѕР±РЅРѕРІР»РµРЅРёРµРј СЃРІСЏР·РµР№
-	int real_size = entire_hull.size() - 2;
+	//Создание моста и возврат к t = -INF с обновлением связей
 	u->next = v;
 	v->prev = u;
-	for (; real_size >= 0; real_size--) {
-		if (entire_hull[real_size]->x <= u->x || entire_hull[real_size]->x >= v->x) {
+	for (int real_size = entire_hull.size() - 2; real_size >= 0; real_size--) {
+		if (entire_hull[real_size]->coord.x <= u->coord.x || entire_hull[real_size]->coord.x >= v->coord.x) {
 			entire_hull[real_size]->step();
 			u = entire_hull[real_size] == u ? u->prev : u;
 			v = entire_hull[real_size] == v ? v->next : v;
@@ -212,7 +214,7 @@ std::vector<point*> search_down_hull(std::vector<point>& vertices, int size, poi
 			entire_hull[real_size]->prev = u;
 			v->prev = entire_hull[real_size];
 			entire_hull[real_size]->next = v;
-			if (entire_hull[real_size]->x < midle->x) {
+			if (entire_hull[real_size]->coord.x < midle->coord.x) {
 				u = entire_hull[real_size];
 			}
 			else {
@@ -223,11 +225,12 @@ std::vector<point*> search_down_hull(std::vector<point>& vertices, int size, poi
 	return entire_hull;
 }
 
-//РџРѕРёСЃРє РІРЅРµС€РЅРµР№ РѕР±РѕР»РѕС‡РєРё
-double search_convex_hull(std::vector<point>& vertices, point* zero) {
+//Поиск внешней оболочки
+double find_convex_hull(std::vector<node_hull>& vertices, node_hull* zero) {
 	std::vector<std::vector<bool>> edges(vertices.size(), std::vector<bool>(vertices.size(), false));
-	std::sort(vertices.begin(), vertices.end());
-	std::vector<point*> half_hull = search_down_hull(vertices, vertices.size(), zero);
+	compare_by_x point_comp;
+	std::sort(vertices.begin(), vertices.end(), point_comp);
+	std::vector<node_hull*> half_hull = find_down_hull(vertices, vertices.size(), zero);
 
 	int k = 0;
 	while (half_hull[k] != zero) {
@@ -253,12 +256,12 @@ double search_convex_hull(std::vector<point>& vertices, point* zero) {
 	}
 
 	for (int i = 0; i < vertices.size(); i++) {
-		vertices[i].z = -vertices[i].z;
+		vertices[i].coord.z = -vertices[i].coord.z;
 	}
 	zero->prev = nullptr;
 	zero->next = nullptr;
 
-	half_hull = search_down_hull(vertices, vertices.size(), zero);
+	half_hull = find_down_hull(vertices, vertices.size(), zero);
 	k = 0;
 	while (half_hull[k] != zero) {
 		half_hull[k]->prev->degree = 0;
@@ -280,19 +283,19 @@ double search_convex_hull(std::vector<point>& vertices, point* zero) {
 }
 
 int main() {
-	point zero;
+	node_hull zero;
 	int n = 0;
-	std::vector<point> vertices;
+	std::vector<node_hull> vertices;
 	double a, b;
 	int size = 0;
 	while (std::cin >> a >> b) {
-		vertices.push_back(point(a, b));
-		vertices[size].rotate(base_angle);
+		vertices.push_back(node_hull{ point{a, b, pow(a, 2) + pow(b, 2)}});
+		vertices[size].coord.rotate(base_angle);
 		vertices[size].index = size;
 		size++;
 	}
 
-	double average_number = search_convex_hull(vertices, &zero);
+	double average_number = find_convex_hull(vertices, &zero);
 	printf("%.6lf\n", average_number);
 
 	return 0;
